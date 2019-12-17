a69ed7b4cdf7bb5364302dd0299e336a0a052bfa
ctf0 <muah003@gmail.com>
2019-12-17 17:16:25 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+- close same folder files
+![group-close](https://user-images.githubusercontent.com/7388088/71008073-b2123900-20f0-11ea-8f42-f01960d17289.gif)
+
+- close all non-dirty files
+![non-dirty](https://user-images.githubusercontent.com/7388088/71008075-b2aacf80-20f0-11ea-9c2a-cb677ba6e887.gif)
+
+## Notes
+
+- until https://github.com/microsoft/vscode/issues/15178 is solved, we have to loop over all opened editors/files
+
+## Issues
+
+- doesnt behave well with groups
--- extension.js
+++ extension.js
@@ -4,23 +4,130 @@ const vscode = require('vscode')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // close_non_dirty
-    const dirty = vscode.commands.registerCommand('extension.close_non_dirty', () => {
-        // get opened editors
-        // check if its "!isDirty"
-        // and close it
+
+    /* ----------------------------- close_non_dirty ---------------------------- */
+    let dirtyDocs = []
+    let dirtyLoop = false
+
+    const dirtyFiles = vscode.commands.registerCommand('extension.close_non_dirty', () => {
+        dirtyLoop = true
+        loopOver(vscode.window.activeTextEditor.document, dirtyDocs)
     })
 
-    // close_folder_files
-    const folder = vscode.commands.registerCommand('extension.close_folder_files', (e) => {
-        let folder = e.fsPath
-        // get opened editors
-        // check if each one filename starts with the "folder" name
-        // if so then close it
+    /* --------------------------- close_folder_files --------------------------- */
+    let folderDocs = []
+    let folderPath = null
+    let folderLoop = false
+
+    const folderFiles = vscode.commands.registerCommand('extension.close_folder_files', (e) => {
+        folderLoop = true
+        folderPath = e.fsPath
+
+        let doc = vscode.window.activeTextEditor.document
+        let name = doc.fileName
+
+        if (inFolder(name, folderPath)) {
+            loopOver(doc, folderDocs)
+        } else {
+            folderDocs.push(name)
+            goNext()
+        }
     })
 
-    context.subscriptions.push(dirty, folder)
+    context.subscriptions.push(dirtyFiles, folderFiles)
+
+    /* ------------------------------- recurssion ------------------------------- */
+    let timmer
+
+    vscode.window.onDidChangeActiveTextEditor((e) => {
+        // stop & reset all
+        if (!e) {
+            clearTimeout(timmer)
+
+            timmer = setTimeout(() => {
+                if (!vscode.window.visibleTextEditors.length && (dirtyLoop || folderLoop)) {
+                    dirtyDocs = []
+                    dirtyLoop = false
+                    folderDocs = []
+                    folderPath = null
+                    folderLoop = false
+                }
+            }, 200)
+        }
+
+        // have files
+        if (e) {
+            clearTimeout(timmer)
+
+            let doc = e.document
+            let name = doc.fileName
+
+            /* ------------------------------- dirty files ------------------------------ */
+            if (dirtyLoop) {
+                // are we back to where we started ?
+                if (isFirstItem(name, dirtyDocs)) {
+                    dirtyDocs = []
+                    dirtyLoop = false
+
+                    return
+                }
+
+                loopOver(doc, dirtyDocs)
+            }
+
+            /* ------------------------------ folder files ------------------------------ */
+            if (folderLoop) {
+                // are we back to where we started ?
+                if (isFirstItem(name, folderDocs)) {
+                    folderDocs = []
+                    folderPath = null
+                    folderLoop = false
+
+                    return
+                }
+
+                if (inFolder(name, folderPath)) {
+                    loopOver(doc, folderDocs)
+                } else {
+                    folderDocs.push(name)
+                    goNext()
+                }
+            }
+        }
+    })
 }
+
+function loopOver(doc, list) {
+    let name = doc.fileName
+
+    if (doc.isDirty) {
+        if (!inList(name, list)) {
+            list.push(name)
+        }
+
+        goNext()
+    } else {
+        vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+        goNext()
+    }
+}
+
+function goNext() {
+    vscode.commands.executeCommand('workbench.action.nextEditor')
+}
+
+function inList(item, list) {
+    return list.some((e) => e == item)
+}
+
+function isFirstItem(item, list) {
+    return list.length && list[0] == item
+}
+
+function inFolder(name, path) {
+    return name.startsWith(path)
+}
+
 exports.activate = activate
 
 function deactivate() { }
